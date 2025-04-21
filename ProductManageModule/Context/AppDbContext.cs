caf7f782f9914a39a306170790b093f61ec4de1f
TuanHHH <hotuan1162@gmail.com>
2025-04-22 00:04:02 +0700

    Thêm giỏ hàng và order

---
+++ ProductManageModule/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductManagementModule.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using ProductManagementModule.Domain;
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Product> products { get; set; }
+        public DbSet<Cart> cart { get; set; }
+        public DbSet<Order> orders { get; set; }
+        public DbSet<Role> role { get; set; }
+        public DbSet<User> user { get; set; }
+        public DbSet<OrderDetail> order_detail { get; set; }
+    }
+}
---
+++ ProductManageModule/Repositories/OrderDetailRepositories.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductManagementModule.Context;
+using ProductManagementModule.Domain;
+using ProductManagementModule.IRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductManagementModule.Repositories
+{
+    public class OrderDetailRepositories : IOrderDetailRepositories
+    {
+        private readonly AppDbContext _dbContext;
+
+        public OrderDetailRepositories(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public void AddRange(IEnumerable<OrderDetail> orderDetails)
+        {
+            _dbContext.order_detail.AddRange(orderDetails);
+            _dbContext.SaveChanges();
+        }
+
+        public OrderDetail GetById(long orderId)
+        {
+            //var cartItem = _dbContext.cart.FirstOrDefault(c => c.UserId == userId && c.ProductId == productId);
+            return _dbContext.order_detail.FirstOrDefault(od => od.OrderId == orderId);
+        }
+    }
+}
